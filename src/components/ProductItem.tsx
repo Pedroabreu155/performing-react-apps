709e0cf7fdc4057029f9bc43f304c48fb802a87c
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-08-21 16:20:45 -0300

    added memo to ProductItem

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react';
+
 type ProductItemProps = {
   product: {
     id: number;
@@ -6,10 +8,17 @@ type ProductItemProps = {
   };
 };
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
